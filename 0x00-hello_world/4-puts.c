fdfadf4c263f1548539052ba11dc5f5e6b5e9391
Aghogho Obukonise <otibux@gmail.com>
2022-09-09 01:54:32 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - entry point 
-* Return: 0 after printing the function
+*
+* main - Entry points
+* Return:always 0 
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	printf("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
